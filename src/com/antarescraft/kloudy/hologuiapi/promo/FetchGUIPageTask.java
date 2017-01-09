5f18fb3ee5ed054ec3529c827ccc4dd4755ed882
Kloudy <kloudy@antarescraft.com>
2017-01-08 20:54:09 -0700

    updated pom

--- src/com/antarescraft/kloudy/hologuiapi/promo/FetchGUIPageTask.java
+++ src/com/antarescraft/kloudy/hologuiapi/promo/FetchGUIPageTask.java
@@ -1,6 +1,13 @@
 package com.antarescraft.kloudy.hologuiapi.promo;
 
+import org.bukkit.scheduler.BukkitRunnable;
+
 public class FetchGUIPageTask extends BukkitRunnable
 {
-
+	private static final String url = "";
+	
+	@Override
+	public void run() 
+	{
+	}
 }
