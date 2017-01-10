fd0b50223effcb282785528b754780e21752cb8f
BuildTools <unconfigured@null.spigotmc.org>
2017-01-10 06:59:49 -0700

    -

--- src/com/antarescraft/kloudy/hologuiapi/promo/HoloGUIPromo.java
+++ src/com/antarescraft/kloudy/hologuiapi/promo/HoloGUIPromo.java
@@ -1,11 +1,40 @@
 package com.antarescraft.kloudy.hologuiapi.promo;
 
+import java.io.StringReader;
+
+import org.bukkit.configuration.file.YamlConfiguration;
+import org.bukkit.entity.Player;
+import org.bukkit.plugin.Plugin;
+
+import com.antarescraft.kloudy.hologuiapi.HoloGUIPlugin;
 import com.antarescraft.kloudy.hologuiapi.guicomponents.GUIPage;
+import com.antarescraft.kloudy.plugincore.io.AsyncTaskCompleteCallback;
+import com.antarescraft.kloudy.plugincore.io.IOManager;
 
 public class HoloGUIPromo 
 {
-	public static GUIPage fetchPromoPage()
+	private static final String url = "https://dl.dropboxusercontent.com/u/49309383/HoloGUI/promo-page.yml";
+	
+	private static GUIPage promoPage;
+	
+	/**
+	 * Fetches the promo page from dropbox
+	 */
+	public static void fetchPromoPage(HoloGUIPlugin plugin)
 	{
-		return null;
+		IOManager.fetchContentFromURL(plugin, url, new AsyncTaskCompleteCallback()
+		{
+			@Override
+			public void call(String content)
+			{
+				YamlConfiguration yaml = YamlConfiguration.loadConfiguration(new StringReader(content));
+				
+			}
+		});
+	}
+	
+	public static void displayPromoPage(Player player)
+	{
+		
 	}
 }
