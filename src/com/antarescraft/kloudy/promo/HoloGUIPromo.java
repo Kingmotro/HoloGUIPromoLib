9a8e80a1a48517287010701473ba87a0a88ac729
Kloudy <kloudy@antarescraft.com>
2017-02-22 20:02:13 -0700

    -

--- src/com/antarescraft/kloudy/promo/HoloGUIPromo.java
+++ src/com/antarescraft/kloudy/promo/HoloGUIPromo.java
@@ -3,12 +3,24 @@ package com.antarescraft.kloudy.promo;
 import org.bukkit.entity.Player;
 
 import com.antarescraft.kloudy.hologuiapi.HoloGUIPlugin;
+import com.antarescraft.kloudy.hologuiapi.guicomponents.GUIPage;
+import com.antarescraft.kloudy.hologuiapi.playerguicomponents.PlayerGUIPageModel;
 
 public class HoloGUIPromo 
 {
-	public static void openPromoPage(HoloGUIPlugin plugin, Player player)
+	public static void openPromoPage(HoloGUIPlugin plugin, Player player, PlayerGUIPageModel model)
 	{
-		PromoPageModel model = new PromoPageModel(plugin, plugin.getGUIPage("promo-page"), player);
-		plugin.getHoloGUIApi().openGUIPage(plugin, player, model);
+		openPromoPage(plugin, player, null, model);
+	}
+	
+	public static void openPromoPage(HoloGUIPlugin plugin, Player player, GUIPage guiPage)
+	{
+		openPromoPage(plugin, player, guiPage, null);
+	}
+	
+	private static void openPromoPage(HoloGUIPlugin plugin, Player player, GUIPage guiPage, PlayerGUIPageModel model)
+	{
+		PromoPageModel promoModel = new PromoPageModel(plugin, plugin.getGUIPage("promo-page"), player, guiPage, model);
+		plugin.getHoloGUIApi().openGUIPage(plugin, player, promoModel);
 	}
 }
--- src/com/antarescraft/kloudy/promo/PromoPageModel.java
+++ src/com/antarescraft/kloudy/promo/PromoPageModel.java
@@ -3,13 +3,43 @@ package com.antarescraft.kloudy.promo;
 import org.bukkit.entity.Player;
 
 import com.antarescraft.kloudy.hologuiapi.HoloGUIPlugin;
+import com.antarescraft.kloudy.hologuiapi.guicomponents.ButtonComponent;
 import com.antarescraft.kloudy.hologuiapi.guicomponents.GUIPage;
+import com.antarescraft.kloudy.hologuiapi.handlers.ClickHandler;
 import com.antarescraft.kloudy.hologuiapi.playerguicomponents.PlayerGUIPageModel;
 
 public class PromoPageModel extends PlayerGUIPageModel
 {
-	public PromoPageModel(HoloGUIPlugin plugin, GUIPage guiPage, Player player)
+	private ButtonComponent continueBtn;
+	private ButtonComponent nextBtn;
+	private ButtonComponent backBtn;
+	
+	private int page = 0;
+	
+	public PromoPageModel(final HoloGUIPlugin plugin, GUIPage guiPage, final Player player, final GUIPage redirectGUIPage, final PlayerGUIPageModel redirectModel)
 	{
 		super(plugin, guiPage, player);
+		
+		continueBtn = (ButtonComponent)guiPage.getComponent("continue-btn");
+		nextBtn = (ButtonComponent)guiPage.getComponent("next-btn");
+		backBtn = (ButtonComponent)guiPage.getComponent("back-btn");
+		
+		continueBtn.registerClickHandler(player, new ClickHandler()
+		{
+			@Override
+			public void onClick()
+			{
+				// Redirect guiPage was passed in
+				if(redirectGUIPage != null)
+				{
+					plugin.getHoloGUIApi().openGUIPage(plugin, player, redirectGUIPage.getId());
+				}
+				// Redirect model was passed in
+				else if(redirectModel != null)
+				{
+					plugin.getHoloGUIApi().openGUIPage(plugin, player, redirectModel);
+				}
+			}
+		});
 	}
 }
